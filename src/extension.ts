e1852547f3ed83cb524b50543627ce5181a6e856
Kazuya Matsumoto <kazuya.matsumoto@icare.jpn.com>
2021-04-12 16:47:47 +0900

    Fix bugs in large yaml files

--- src/extension.ts
+++ src/extension.ts
@@ -11,70 +11,90 @@ import {
   MarkdownString,
   Location,
   Range,
-  Uri
+  Uri,
+  CompletionItemKind
 } from 'vscode';
 import { parse } from 'yaml';
 import { Parser } from "./parser";
-import { flattenDeep, isUndefined } from 'lodash';
+import { flattenDeep, isUndefined, isEmpty, compact, isNull } from 'lodash';
 
 
 export async function activate(context: ExtensionContext) {
   let yamlItems: YamlItem[] = await parseYamlFiles();
+  const completions = compact(yamlItems.map((i) => {
+    try{
+      // NOTE:
+      // to work completion in string(e.g literal("xxx.yyy"))
+      // Turn on setting editor.quickSuggestions: true
+      // "editor.quickSuggestions": {
+      //   ...
+      //   "strings": true
+      // }
+      if(isEmpty(i.value) || isEmpty(i.key)){
+        return null;
+      }
+  
+      const completion = new CompletionItem(i.key, CompletionItemKind.Text);
+      completion.documentation = new MarkdownString(i.value.toString());
+      return completion;
+
+    } catch(err) {
+      return null;
+    }
+  }));
 
   // CompletionProvider
   const completionProvider = languages.registerCompletionItemProvider('javascript', {
     provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken, context: CompletionContext) {
-      const completions = yamlItems.map((i) => {
-        // NOTE:
-        // to work completion in string literal("xxx.yyy")
-        // Turn on setting editor.quickSuggestions: true
-        // "editor.quickSuggestions": {
-        //   ...
-        //   "strings": true
-        // }
-        const completion = new CompletionItem(i.key);
-        completion.documentation = new MarkdownString(i.value);
-        return completion;
-      });
-      const completion = new CompletionItem("\"nyanko\"");
-      return [...completions, completion];
+      return completions;
     }
   });
 
   // Definition
   const definitionProvider = languages.registerDefinitionProvider({ scheme: 'file', language: 'javascript' }, {
     provideDefinition(document: TextDocument, position: Position, token: CancellationToken) {
-      // Read focused line
-      const line = document.lineAt(position);
+      try{
+        // Read focused line
+        const line = document.lineAt(position);
 
-      // Try to extract yaml key with Regex
-      const result = line.text.match(/.*i18n.t\("(?<yamlKey>.*)"\).*/);
-      const yamlKey = result?.groups?.yamlKey;
-      // Return if not yamlKey
-      if (isUndefined(yamlKey)) { return; }
+        // Try to extract yaml key with Regex
+        // TODO: hanlde single quatation
+        const result = line.text.match(/.*i18n.t\("(?<yamlKey>.*)"\).*/);
+        const yamlKey = result?.groups?.yamlKey;
+        // Return if not yamlKey
+        if (isUndefined(yamlKey)) { 
+          console.log("yamlKey not found", line.text);
+          return; 
+        }
 
-      // Find corresponding yaml item by yamlKey
-      const item = yamlItems.find((item) => item.key === yamlKey);
-
-      // TODO: Showing no yaml key
-      if (isUndefined(item)) { return; }
-
-      const range = new Range(new Position(item.lineNumber, 0), new Position(item.lineNumber, 0));
-      const location = new Location(Uri.file(item.path), range);
-      return location;
+        // Find corresponding yaml item by yamlKey
+        const item = yamlItems.find((item) => item.key === yamlKey);
+        // TODO: Showing no yaml key
+        if (isUndefined(item)) { 
+          console.log("item not found", yamlKey);
+          return; }
+  
+        const range = new Range(new Position(item.lineNumber, 0), new Position(item.lineNumber, 0));
+        const location = new Location(Uri.file(item.path), range);
+        return location;
+      } catch(err) {
+        console.error("err", err);
+      }
     }
-  });
 
+  });
+  console.log("yaml-X: Loaded");
   context.subscriptions.push(completionProvider, definitionProvider);
 }
 
+// TODO: トップのenやjaを落とす対応を落とす対応
 const parseYamlFiles = async (): Promise<YamlItem[]> => {
   // Get WorkSpaceFolder
   const folder = workspace.workspaceFolders?.[0];
   if (!folder) { return []; }
 
   // List yml,yaml file uris in the folder
-  const relativePath = new RelativePattern(folder, '**/*.{yml,yaml}');
+  const relativePath = new RelativePattern(folder, 'config/locales/**/*.{yml,yaml}');
   const uris = await workspace.findFiles(relativePath);
 
   // Parse yaml files to YamlItems
@@ -86,7 +106,7 @@ const parseYamlFiles = async (): Promise<YamlItem[]> => {
       return items;
     })
   );
-  return flattenDeep(items);
+  return compact(flattenDeep(items));
 };
 
 // this method is called when your extension is deactivated
