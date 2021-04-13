bb936bc0c45096f0db164571b0dfa1c9692ef912
kazuwombat <matsumotokazuya7@gmail.com>
2021-04-14 00:10:01 +0900

    Rewrite parser: multiRoot

--- src/test/unit/parser.test.ts
+++ src/test/unit/parser.test.ts
@@ -67,5 +67,42 @@ describe('Parser', function () {
       ];
       assert.deepStrictEqual(result, expected);
     });
+    it('multiRoot.yaml', function () {
+      // Prepare
+      const filePath = path.resolve(__dirname, "../../../sample-yml/multiRoot.yaml");
+      const yml = fs.readFileSync(filePath, 'utf8');
+
+      // Execute
+      const result = new Parser(filePath).parse(yml);
+
+      // Evaluate
+      const expected: YamlItem[] = [
+        {
+          key: "hoge",
+          value: null,
+          lineNumber: 1,
+          path: filePath
+        },
+        {
+          key: "hoge.baz",
+          value: "Baz",
+          lineNumber: 2,
+          path: filePath
+        },
+        {
+          key: "piyo",
+          value: null,
+          lineNumber: 3,
+          path: filePath
+        },
+        {
+          key: "piyo.foo",
+          value: "Foo",
+          lineNumber: 4,
+          path: filePath
+        }
+      ];
+      assert.deepStrictEqual(result, expected);
+    });
   });
 });
